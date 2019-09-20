d85275052d6071db3499a65c45f433628f46850e
Wizkels <43842402+Wizkels@users.noreply.github.com>
2019-09-20 13:57:35 +0200

    primera clase

---
+++ Assets/_gameobject/scripts/Pollo.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pollo : MonoBehaviour {
+    // Start is called before the first frame update
+    void Start() {
+        print("en el start");
+    }
+
+    // Update is called once per frame
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            print("salta");
+        }
+    }
+}
